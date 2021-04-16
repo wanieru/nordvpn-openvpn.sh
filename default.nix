d8a3ab5e64c0829f4c5c238d316e31a6aa2d0d10
SEbbaDK <sebbadk@gmail.com>
2021-04-16 13:43:25 +0200

    Add makeWrapper wrapping in nix derivation

--- default.nix
+++ default.nix
@@ -1,5 +1,7 @@
 { pkgs ? import <nixpkgs> { }
 , mkDerivation ? pkgs.stdenv.mkDerivation
+, makeWrapper ? pkgs.makeWrapper
+, lib ? pkgs.lib
 , unzip ? pkgs.unzip
 , wget ? pkgs.wget
 , openvpn ? pkgs.openvpn
@@ -9,11 +11,12 @@ pkgs.stdenv.mkDerivation {
   name = "nordvpn";
   src = ./.;
 
+  buildInputs = [ makeWrapper ];
   nativeBuildInputs = [ unzip wget openvpn ];
 
   installPhase = ''
     mkdir -p $out/bin
-    mv nordvpn.sh $out/bin/
-    chmod +x $out/bin/nordvpn.sh
+    cp nordvpn.sh $out/bin/.nordvpn-wrapped
+    makeWrapper $out/bin/.nordvpn-wrapped $out/bin/nordvpn --prefix PATH : ${lib.makeBinPath [ unzip wget openvpn ]}
   '';
 }
